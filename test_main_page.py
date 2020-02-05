e40b74c9dec6c8338b5231da7ddb0614a340d9e8
StarVas <vasyl.starykov@gmail.com>
2020-02-05 11:42:29 +0200

    latest

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,9 @@
 from Pages.main_page import MainPage
 
-
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
+    page = MainPage(browser, link)
+    page.open()
     page.go_to_login_page()
+    page.should_be_login_form()
+    page.should_be_register_form()
