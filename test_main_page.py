bbc756cfc0e86835abb24c60f8f21de9571f2657
StarVas <vasyl.starykov@gmail.com>
2020-02-06 08:49:57 +0200

    latest tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,10 @@
 from Pages.main_page import MainPage
+from Pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    page.should_be_login_form()
-    page.should_be_register_form()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
