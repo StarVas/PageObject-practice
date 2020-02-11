a653cb8a014371e29f317d3457439182ea2fe48f
StarVas <vasyl.starykov@gmail.com>
2020-02-11 15:52:55 +0200

    updated locators

--- Pages/locators.py
+++ Pages/locators.py
@@ -7,4 +7,8 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "form#login_form.well")
     REGITSER = (By.CSS_SELECTOR, "form#register_form.well")
+class PageObjectLocators():
+    CLICK_THE_BUTTON = (By.CSS_SELECTOR, "button.btn.btn-lg.btn-primary.btn-add-to-basket") 
+    ITEM_NAME=(By.CSS_SELECTOR,'div.alertinner strong')
+    NAME=(By.CSS_SELECTOR,"div h1")  
     
